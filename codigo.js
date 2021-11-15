eb0619d855125fc870fc563948019e5724412034
Mayra18 <romeromayraangie@gmail.com>
2021-11-15 17:42:00 -0600

    cambios

---
+++ codigo.js
@@ -0,0 +1,10 @@
+
+const txt = document.getElementById("text") ;
+const cot = document.getElementById("contador") ;
+const cdg = document.getElementById("codigos") ;
+const btnc = document.getElementById("btn-cifrar") ;
+const btnd = document.getElementById("btn-descifrar");
+const result = document.getElementById("mjsresult") ;
+const txr = document.getElementById("text-result") ;
+const btncopy = document.getElementById("copy") ;
+const contenedor = document.getElementsByClassName("contenedor") ;
